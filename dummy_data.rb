65ab3da27d24369a5ee874ac90b4d402c650a828
JD Pagano <jonathanpagano@gmail.com>
2016-12-11 18:07:59 -0500

    Update dummy data

--- dummy_data.rb
+++ dummy_data.rb
@@ -38,16 +38,16 @@ def build_dummy_data
   q3 = Question.create(stem: 'Do you have a stable home residence?', order: 3)
   Choice.create(stem: 'Yes', question: q3)
   ch = Choice.create(stem: 'No', question: q3)
-  ch.update(tag_pks: [tags[2].id])
+  ch.update(tag_pks: [tags[2].id, tags[3].id])
 
   q2 = Question.create(stem: 'Are you actively taking narcotic?', order: 2)
   ch = Choice.create(stem: 'Yes', question: q2, next_question: q3)
-  ch.update(tag_pks: [tags[1].id])
+  ch.update(tag_pks: [tags[1].id, tags[3].id])
   Choice.create(stem: 'No', question: q2, next_question: q3)
 
   q1 = Question.create(stem: 'Are you HIV positive?', order: 1)
   ch = Choice.create(stem: 'Yes', question: q1, next_question: q2)
-  ch.update(tag_pks: [tags[0].id])
+  ch.update(tag_pks: [tags[0].id, tags[3].id])
   Choice.create(stem: 'No', question: q1, next_question: q2)
 
 end
